ef636bca516b47fcec799fda6ba85d2195d9b385
sayhan1610 <sayhanrahman16@gmail.com>
2025-08-17 23:48:03 +0600

    Newsletter Button Added to All Pages

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-    const btn = document.getElementById('subscribeBtn');
-    const overlay = document.getElementById('overlay');
-    const closeModal = document.getElementById('closeModal');
-    const hint = document.getElementById('subscribeHint');
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('subscribeBtn');
+  const overlay = document.getElementById('overlay');
+  const closeModal = document.getElementById('closeModal');
+  const hint = document.getElementById('subscribeHint');
 
-    btn.addEventListener('click', () => {
-      overlay.classList.add('active');
-      hint.style.display = 'none';
-    });
+  btn.addEventListener('click', () => {
+    overlay.classList.add('active');
+    hint.style.display = 'none';
+  });
 
-    closeModal.addEventListener('click', () => {
-      overlay.classList.remove('active');
-    });
+  closeModal.addEventListener('click', () => {
+    overlay.classList.remove('active');
+  });
 
-    setTimeout(() => {
-      hint.style.opacity = '0';
-      setTimeout(() => hint.remove(), 500);
-    }, 5000);
+  setTimeout(() => {
+    hint.style.opacity = '0';
+    setTimeout(() => hint.remove(), 500);
+  }, 5000);
+
+   (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1740404');
+});
